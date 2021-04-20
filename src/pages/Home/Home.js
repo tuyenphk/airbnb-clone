65e92601c8e5e5a70865bde92160764eeec414dd
Tuyen Pham <tuh39709@temple.edu>
2021-04-20 13:44:57 -0400

    setup banner

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Banner from '../../components/Banner/Banner'
 
 function Home() {
     return (
         <div className="home">
             <h1>Home Page</h1>
+            <Banner />
         </div>
     )
 }
