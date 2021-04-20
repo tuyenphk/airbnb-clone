e3a0630711abfa541847cacc0c6eefde4cabecf8
Tuyen Pham <tuh39709@temple.edu>
2021-04-20 16:34:59 -0400

    get result page without referesing

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,9 +2,11 @@ import React, {useState} from 'react'
 import './Banner.css'
 import { Button } from '@material-ui/core'
 import Search from '../Search/Search'
+import { useHistory } from 'react-router';
 
 function Banner() {
     const [showSearch, setShowSearch] = useState(false);
+    const history = useHistory()
 
     return (
         <div className="banner">
@@ -21,7 +23,7 @@ function Banner() {
             <div className="banner-info">
                 <h1>Get out and stretch your imagination</h1>
                 <h5>Plan a different kind of getaway to uncover the hidden gems near you</h5>
-                <Button variant="outlined">Explore Nearby</Button>
+                <Button variant="outlined" onClick={()=>history.push('/search')}>Explore Nearby</Button>
             </div>
         </div>
     )
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,16 @@ import SearchIcon from '@material-ui/icons/Search';
 import LanguageIcon from '@material-ui/icons/Language';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import {Link} from 'react-router-dom'
 
 function Header() {
     return (
         <div className="header">
-            <img className="header-icon"
-                 src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-                 alt="" />
+            <Link to="/">
+                <img className="header-icon"
+                    src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
+                    alt="" />
+            </Link>
 
             <div className="header-center">
                 <input type="text" />
